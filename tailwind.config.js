9bda30e78b4ef935b1d11252945621cf15f3de20
ReshMadeIt <reshmadeit@gmail.com>
2026-04-19 08:33:55 +0300

    Add components directory to Tailwind CSS content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
     "./pages/**/*.{js,jsx,ts,tsx,mdx}",
     "./src/**/*.{js,jsx,ts,tsx,mdx}",
   ],
