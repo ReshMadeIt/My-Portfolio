051c7ce13b820529efa10c3f5f5bb444c09a33c1
ReshMadeIt <reshmadeit@gmail.com>
2026-05-01 04:13:30 +0300

    Correct portfolio title spelling

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export const metadata = {
-  title: "Reshley Atsaiya - Portfolio",
+  title: "Reshley Atsiaya - Portfolio",
   icons: {
     icon: "icons/science.png",
   },
